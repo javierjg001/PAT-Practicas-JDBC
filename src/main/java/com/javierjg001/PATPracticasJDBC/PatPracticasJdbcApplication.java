7541c26cb34ead859c75f63c4c9191a17b1b5a7e
javierjg001 <56931870+javierjg001@users.noreply.github.com>
2022-04-17 18:53:15 +0200

    Primera subida

---
+++ src/main/java/com/javierjg001/PATPracticasJDBC/PatPracticasJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.javierjg001.PATPracticasJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatPracticasJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatPracticasJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javierjg001/PATPracticasJDBC/PatPracticasJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierjg001.PATPracticasJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatPracticasJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
